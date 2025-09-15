b66975715ef2d35518253782634fba9a2d91a4f1
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-09-15 12:18:30 +0530

    rate limiting and cors added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,35 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule, OrdersModule], // Import the new Modules
+  imports: [
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+    OrdersModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ], // Import the new Modules
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}

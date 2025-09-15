8e881154d3066800eff1208512b88bd981b36589
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-09-15 17:42:04 +0530

    estjs/throttler note added

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,3 +43,5 @@ import { APP_GUARD } from '@nestjs/core';
 export class AppModule {}
 
 // Using @nestjs/config is a critical step in making your application ready for different environments (development, testing, production) without changing the underlying code.
+
+// The @nestjs/throttler package is a NestJS module that provides a rate-limiting guard to control the number of requests a client can make to your API within a specific timeframe. 🚦 This helps protect your application from brute-force attacks, denial-of-service (DoS) attacks, and general misuse by malicious actors or bots.
